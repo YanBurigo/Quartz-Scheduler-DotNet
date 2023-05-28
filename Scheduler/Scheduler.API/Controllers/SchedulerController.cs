819fd101b7a8759c3d2c61ef8aaaff638e1eb5a7
YanBurigo <yanburigoribeiro@gmail.com>
2023-05-28 15:23:08 -0300

    Initial creation of the webapi

---
+++ Scheduler/Scheduler.API/Controllers/SchedulerController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Scheduler.API.Controllers
+{
+    [ApiController]
+    [Route("api/v1/[controller]")]
+    public class SchedulerController : ControllerBase
+    {
+        [HttpGet("is-alive")]
+        public IActionResult IsAlive()
+        {
+            return Ok(true);
+        }
+    }
+}
